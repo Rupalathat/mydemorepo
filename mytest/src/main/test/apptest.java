2893e604868be92594488286e6faf61f5702043e
Rupalathat <151708642+Rupalathat@users.noreply.github.com>
2025-02-22 06:46:08 +0000

    updating code

---
+++ mytest/src/main/test/apptest.java
@@ -0,0 +1,9 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    @Test
+    public void testMain() {
+        assertEquals(1, 1); // Simple assertion
+    }
+}
